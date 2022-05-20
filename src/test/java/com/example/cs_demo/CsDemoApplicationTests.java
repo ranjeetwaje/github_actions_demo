108cd77627b7b20605251a2932470d861cc05329
ranjeetwaje <ranjeet.waje@gmail.com>
2022-05-20 14:10:41 +0530

    Initial code commit with workflow changes

---
+++ src/test/java/com/example/cs_demo/CsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
